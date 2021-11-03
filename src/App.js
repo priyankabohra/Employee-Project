374bd683ed285f9b119c20b507f1c0bc4f802383
priyankabohra <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>
2021-11-04 00:19:19 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import EmployeeList from './components/EmployeeList';
 import AddEmployee from './components/AddEmployee';
 import ViewEmployee from './components/ViewEmployee';
+import Header from './components/Header';
+
 function App() {
         return (
             <div>
+            <Header/>
                 <Router>
 
                         <div className="container">
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">Employee Management System </div>
+  );
+}
+export default Header;
